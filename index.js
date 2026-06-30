85fef00c5ba2308313e18b75e1ffe8fd8dbb5e5a
Arpit Bind <codearchitecture79@gmail.com>
2026-06-30 10:07:24 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import userRoutes from './src/routes/users.js';
 import { errorHandler } from './src/middleware/errorHandler.js';
+import cors from 'cors'
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
